7e9dff96d25f9b0522ca36a7965bf19a92ce9291
Martinez <martin.martinez@softwareone.com>
2022-11-24 12:53:53 +0100

    ejemplo multitabla

---
+++ multitablaspring/src/main/java/com/multitablaspring/multitablaspring/MultitablaspringApplication.java
@@ -0,0 +1,13 @@
+package com.multitablaspring.multitablaspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MultitablaspringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MultitablaspringApplication.class, args);
+	}
+
+}
---
+++ multitablaspring/src/test/java/com/multitablaspring/multitablaspring/MultitablaspringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.multitablaspring.multitablaspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MultitablaspringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
